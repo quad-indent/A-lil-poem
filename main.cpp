e90d7da0a251bed96d8ea4deb09217c115a8e287
quad-indent <119438108+quad-indent@users.noreply.github.com>
2024-06-02 13:21:00 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define up int
+#define find char
+#define take std::cin
+#define let for
+#define Hermes (
+#define carry auto
+#define unto :
+#define in ==
+#define there if
+#define is (
+#define skies 0
+#define we ++
+#define to std::cout
+#define soar <<
+#define the =
+#define rise ;
+#define Shall const
+#define awaits main
+#define O }
+#define come )
+#define then {
+
+Shall find Olympus[] = "Let's count letter occurrences in this string";
+
+up awaits Hermes
+
+    come then
+    find me = ' ';
+
+    take >> me; up above the skies;
+
+    let Hermes carry us unto Olympus) {
+        there is me in us)
+            above we rise
+
+    O to soar above;
+}
